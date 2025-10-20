29eaae0c9b7c3271ed5ad4a97c4779f754fc45e1
Amir1gh <amirhossien.gholami14@gmail.com>
2025-10-20 17:52:04 +0330

    Update settings.py

--- digikala/settings.py
+++ digikala/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','fallback-secret')
+SECRET_KEY = 'django-insecure-5kpws7et_t^f_f-mya*4_a-t$s-)&$@86o(5f-n-!^d3)&sq@g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG','False') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = ['digikalaamirshop.onrender.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['https://digikala3.onrender.com']
 
 
 # Application definition
@@ -82,7 +82,10 @@ WSGI_APPLICATION = 'digikala.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
@@ -136,7 +139,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
     ]
 
-
+STATICFILES_URLS = ['static/']
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
